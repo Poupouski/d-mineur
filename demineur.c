70559c5f51952dfe6d0ae2a699469273f203ada1
Poupouski <105277484+Poupouski@users.noreply.github.com>
2022-05-16 22:29:08 +0200

    début démineur en dessous
    fonction de la grille débutant + tableau des mines

--- demineur.c
+++ demineur.c
@@ -85,3 +85,88 @@ int main(){
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+char JEU[10][10];
+char mine[10][10];
+int i,j;
+
+
+void GRILLEdeb(){
+    printf("   A B C D E F G H I J\n");
+    for(i=0;i<10;i++){
+        printf("%d |",i);
+        for(j=0;j<10;j++){
+            printf("%c|",JEU[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    int c=10;
+    int i,j;
+    srand(time(NULL));
+    
+    for(i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            JEU[i][j]='~';
+            mine[i][j]='0';
+        }
+    }
+    
+    for(i=0;i<c;i++){
+        i=rand()%10;
+        j=rand()%10;
+        if(mine[i][j]=='0'){
+            mine[i][j]='M';
+        }
+        else{
+            c++;
+        }
+   }
+   
+    
+    GRILLEdeb();
+    printf("\n\n");
+    for(i=0;i<10;i++){
+        for(j=0;j<10;j++){
+            printf(" %c",mine[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+
+
+
